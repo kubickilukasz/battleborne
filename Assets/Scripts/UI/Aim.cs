bae1c6c5f44054a27fab947363be0158ab09f413
Kubeec <kubicki.lukasz98@gmail.com>
2021-04-26 20:44:38 +0200

    Menu w trakcie gry, dalsze UI,

--- Assets/Scripts/UI/Aim.cs
+++ Assets/Scripts/UI/Aim.cs
@@ -7,12 +7,47 @@ public class Aim : MonoBehaviour
 {
 
     [SerializeField]
-    JetMovement movement;
+    JetMovement movement; // TODO zmienić na JetShooting
 
     [SerializeField]
     RectTransform leftAim;
 
     [SerializeField]
     RectTransform rightAim;
+
+    [SerializeField]
+    float smooth;
+
+    [SerializeField]
+    float maxSpeed;
+
+    [SerializeField]
+    float focusPosX = 70f;
+
+    [SerializeField]
+    float recoilPosX = 150f;
+
+    [SerializeField]
+    bool isFocusing = true;
+
+    float refCurrentVelocity;
+    Vector3 tempPosition;
+
+    void Start(){
+        tempPosition = new Vector3(leftAim.anchoredPosition.x,0,0);
+    }
+
+    void Update()
+    {
+        if(isFocusing){
+            tempPosition = new Vector3(Mathf.SmoothDamp(tempPosition.x, focusPosX, ref refCurrentVelocity, smooth, maxSpeed),0,0);
+        }else{
+            tempPosition = new Vector3(Mathf.SmoothDamp(tempPosition.x, recoilPosX, ref refCurrentVelocity, smooth, maxSpeed),0,0);
+        }
+        leftAim.anchoredPosition = -tempPosition;
+        rightAim.anchoredPosition = tempPosition;
+    }
+
+
 
 }
